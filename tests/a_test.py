368090f3a4ba290b348811bd343b1f438ab22581
Krzysztof Kępiński <krzysztof.kepinski@it-krzysztofkepinski.local>
2019-05-17 18:09:51 +0200

    cleanup test code with fixtures

--- tests/a_test.py
+++ tests/a_test.py
@@ -1,49 +1,47 @@
 
 import pytest
 
+#test fixtures
+@pytest.fixture
+def measure_lines():
+  measure = WithLines()
+  return ComplexityMeter(measure)
+
+@pytest.fixture
+def measure_indents():
+  measure = WithIndents()
+  return ComplexityMeter(measure)
+
 #test code
-def test_empty_inputs_list_returns_empty_results_list():
-  measure = WithLines()
-  system = ComplexityMeter(measure)
-  input = []
-  result = system.measure(input)
-  assert result == []
+def test_empty_inputs_list_returns_empty_results_list(measure_lines):
+  assert measure_lines.measure([]) == []
 
-def test_single_input_returns_single_measure():
-  measure = WithLines()
-  system = ComplexityMeter(measure)
-  input = [""]
-  result = system.measure(input)
-  assert len(result) == 1
-
+def test_single_input_returns_single_measure(measure_lines):
+  assert len(measure_lines.measure([""])) == 1
 
 @pytest.mark.parametrize(
   "file,                              expected_value",[
   ("",                                0),
-  ("  ",                              1),
+  ("  ",                              1),#??
   ("one_liner",                       1),
   ("one_line\nline 2\nline 3",        3),
   ("one_line\nline 2\n",              2),
   ("\n\n\n\n\n\n",                    0)
 ])
-def test_code_lines_complexity(file, expected_value):
-  measure = WithLines()
-  system = ComplexityMeter(measure)
-  assert system.measure([file]) == [expected_value]
+def test_code_lines_complexity(file, expected_value, measure_lines):
+  assert measure_lines.measure([file]) == [expected_value]
 
 @pytest.mark.parametrize(
   "file,                              expected_value",[
   ("",                                0),
-  ("  ",                              0),
-  ("one_line\nline 2\nline 3",        0),
+  ("  ",                              0),#??
+  ("one_line\nline 2\nline 3",        0),#??
   ("\n\n\n\n\n\n",                    0),
   ("\tline",                          1),
   ("\t\tline",                        2)
 ])
-def test_code_indents_complexity(file, expected_value):
-  measure = WithIndents()
-  system = ComplexityMeter(measure)
-  assert system.measure([file]) == [expected_value]
+def test_code_indents_complexity(file, expected_value, measure_indents):
+  assert measure_indents.measure([file]) == [expected_value]
 
 #production code
 
